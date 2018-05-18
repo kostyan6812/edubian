38bd4dda8fe7b74ad69ea0c4f76fb020d59ef976
kostyan6812 <kostyan6812@yandex.ru>
2018-05-18 10:47:28 +0300

    change start script docker_edubian.sh

--- docker_edubian.sh
+++ docker_edubian.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-sudo docker run --privileged --cap-add=SYS_ADMIN --cap-add=MKNOD --cap-add=SYS_PTRACE -v ${PWD}/output:/root/output -it edubian:1.1 -c "export TERM=$TERM" -d --rm
+sudo docker run --privileged --cap-add=SYS_ADMIN --cap-add=MKNOD --cap-add=SYS_PTRACE -v ${PWD}/output:/root/output -e TERM=$TERM --rm -it edubian:1.1
