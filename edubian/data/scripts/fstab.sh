971709a6922a07d345fc4052a631fc450602c1ac
kostyan6812 <kostyan6812@yandex.ru>
2019-01-29 15:30:37 +0300

    Update fstab.sh
    fix

--- edubian/data/scripts/fstab.sh
+++ edubian/data/scripts/fstab.sh
@@ -1,4 +1,13 @@
-#!/bin/bash
+#!/bin/sh -e
+### BEGIN INIT INFO
+# Provides:             fstab
+# Required-Start:       
+# Required-Stop:        
+# Should-Start:
+# Default-Start:        S
+# Default-Stop:         0 1 6
+# Short-Description:    fstab
+### END INIT INFO
 
 blkid >> /etc/fstab
 update-rc.d -f fstab.sh remove
