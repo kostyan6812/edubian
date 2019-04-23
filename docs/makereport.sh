7269a8a26b78b3601bdd893f798d25a8b294edac
kostyan6812 <kostyan6812@yandex.ru>
2019-04-23 08:48:09 +0300

    Create makereport.sh
    create makereport script

---
+++ docs/makereport.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+##prepare for report
+cd ${HOME}
+mkdir -v report || echo 'dir exist, skip'
+
+export EDUBIANUSERPROFILE=${HOME}/Assignment/.${USER}
+
+## convert notebook to htm
+for file in Assignment/Topics/*.ipynb; do
+  echo ${file%.*}.html
+  sed -i '/User/ s/.$/\\n",/' $file
+  sed -i "/User/a\"username ${USER}\"" $file
+  jupyter-nbconvert $file --to html --template edubian
+  mv ${file%.*}.html ${HOME}/report
+done
+
+## convert html to docx
+for file in report/*.html; do
+  pandoc $file --to=docx -o ${file%.*}.docx
+  rm -vf $file 
+done
+
+## create archive
+tar -czvf ${USER}.tar.gz report/
+
+##clear
+rm -rfv report
